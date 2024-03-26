f32f9e14a9270feaaf3ad851ae75b1b295e513a9
Valentina Tevyants <vteviants@neoflex.ru>
2024-03-26 10:34:46 +0300

    init

---
+++ src/main/java/ru/astroconv/creditconveyorastro/CreditConveyorAstroApplication.java
@@ -0,0 +1,13 @@
+package ru.astroconv.creditconveyorastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditConveyorAstroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditConveyorAstroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/astroconv/creditconveyorastro/CreditConveyorAstroApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.astroconv.creditconveyorastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditConveyorAstroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
